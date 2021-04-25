c2fe7b6a71ff28f3760a1a500f40a1fe0cbe8df6
Azor <i@azor.net>
2021-04-25 21:11:22 +0200

    Add progress bar

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,23 @@
 use structopt::StructOpt;
+use indicatif::{ProgressBar as IndicatifBar, ProgressStyle};
+
+pub struct ProgressBar {
+    bar: IndicatifBar
+}
+
+impl ProgressBar {
+    pub fn new() -> ProgressBar {
+        let progress_bar = IndicatifBar::new_spinner();
+        progress_bar.set_style(ProgressStyle::default_bar().template("[{elapsed_precise}] {pos} attempts"));
+        progress_bar.set_draw_delta(100);
+
+        ProgressBar { bar: progress_bar }
+    }
+
+    pub fn tick(&self) {
+        self.bar.inc(1);
+    }
+}
 
 #[derive(StructOpt)]
 #[structopt(rename_all = "kebab-case")]
@@ -12,3 +31,4 @@ impl Cli {
         Cli::from_args()
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@ mod eth;
 mod cli;
 
 use eth::KeyPair;
-use cli::Cli;
+use cli::{Cli, ProgressBar};
 
 fn main() {
-    let cli_args = Cli::new();
 
-    println!("Working...");
+    let cli_args = Cli::new();
+    let progress_bar = ProgressBar::new();
+
     loop {
         let pair = KeyPair::generate();
+
+        progress_bar.tick();
         if &pair.address[..cli_args.starts_with.len()] == cli_args.starts_with {
-            println!("\n");
             println!("Private key: {}", pair.get_private_key_as_hex());
             println!("Address: {}", pair.get_address_with_prefix());
+            println!("\n");
         }
     }
 }
