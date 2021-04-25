f33041994241558c387528247866c892c2f34a64
Azor <i@azor.net>
2021-04-25 22:05:47 +0200

    Switch to parallel iterator

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,26 @@ mod cli;
 
 use eth::KeyPair;
 use cli::{Cli, ProgressBar};
+use rayon::iter::ParallelIterator;
 
 fn main() {
-
     let cli_args = Cli::new();
     let progress_bar = ProgressBar::new();
 
-    loop {
-        let pair = KeyPair::generate();
-
-        progress_bar.tick();
-        if &pair.address[..cli_args.starts_with.len()] == cli_args.starts_with {
-            println!("Private key: {}", pair.get_private_key_as_hex());
-            println!("Address: {}", pair.get_address_with_prefix());
-            println!("\n");
-        }
-    }
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(num_cpus::get())
+        .build()
+        .unwrap()
+        .install(|| {
+            rayon::iter::repeat(KeyPair::generate)
+                .for_each(|generate| {
+                    let pair = generate();
+                    progress_bar.tick();
+                    if &pair.address[..cli_args.starts_with.len()] == cli_args.starts_with {
+                        println!("Private key: {}", pair.get_private_key_as_hex());
+                        println!("Address: {}", pair.get_address_with_prefix());
+                        println!("\n");
+                    }
+                })
+        });
 }
