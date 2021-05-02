27aef5b4621c2dc6a151dcc47fe68dd28a84b85b
Azor <azor@Mac-mini.home>
2021-05-02 11:18:53 +0200

    Add pattern matching option

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,7 @@
 use structopt::StructOpt;
 use indicatif::{ProgressBar as IndicatifBar, ProgressStyle};
+use std::process;
+use regex::Regex;
 
 pub struct ProgressBar {
     bar: IndicatifBar
@@ -19,16 +21,73 @@ impl ProgressBar {
     }
 }
 
+pub enum Mode {
+    StartsWith,
+    Match
+}
+
 #[derive(StructOpt)]
 #[structopt(rename_all = "kebab-case")]
 pub struct Cli {
+    /// Matches on addresses that starts with given chars. Example: dead69
     #[structopt(long)]
-    pub starts_with: String,
+    pub starts_with: Option<String>,
+
+    /// Matches on a given pattern where X equals any char. Example: deadXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX69
+    #[structopt(long="match")]
+    pub match_value: Option<String>,
 }
 
 impl Cli {
     pub fn new() -> Cli {
         Cli::from_args()
     }
+
+    pub fn get_mode(&self) -> (Mode, String) {
+        if self.starts_with.is_some() {
+            let val = self.starts_with.as_ref().unwrap();
+            validate_hex(val);
+            return (Mode::StartsWith, String::from(val))
+        }
+
+        if self.match_value.is_some() {
+            let val = self.match_value.as_ref().unwrap();
+            let pattern = validate_and_format_pattern(val);
+
+            (Mode::Match, pattern)
+        }
+
+        else { 
+            exit_with_err("Select a valid option. For more information try --help.".to_string());
+            panic!();
+        }
+    }
+
 }
 
+fn exit_with_err(msg: String) {
+    println!("{}", msg);
+    process::exit(1)
+}
+
+fn validate_hex(s: &String) {
+    let re = Regex::new(r"^[a-fA-F0-9]*$").unwrap();
+    if !re.is_match(s) {
+        exit_with_err(format!("The value {} is not a valid hex.", s));
+    }
+}
+
+fn validate_and_format_pattern(pattern: &String) -> String {
+    let pattern_length = 40;
+
+    if pattern.len() != pattern_length {
+        exit_with_err(format!("Pattern length must be {} chars.", pattern_length));
+    }
+
+    let re = Regex::new(r"^[a-fA-F0-9X]*$").unwrap();
+    if !re.is_match(pattern) {
+        exit_with_err(format!("Invalid pattern syntax: {}. For more information try --help.", pattern));
+    }
+
+    pattern.replace("X", ".")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod eth;
 mod cli;
 
+use std::str::FromStr;
 use eth::KeyPair;
-use cli::{Cli, ProgressBar};
+use cli::{Cli, ProgressBar, Mode};
 use rayon::iter::ParallelIterator;
+use regex::Regex;
 
 fn main() {
     let cli_args = Cli::new();
+    let mode = cli_args.get_mode();
     let progress_bar = ProgressBar::new();
 
     rayon::ThreadPoolBuilder::new()
@@ -18,7 +21,13 @@ fn main() {
                 .for_each(|generate| {
                     let pair = generate();
                     progress_bar.tick();
-                    if &pair.address[..cli_args.starts_with.len()] == cli_args.starts_with {
+
+                    let is_match = match &mode {
+                        (Mode::StartsWith, input_value) => &pair.address[..input_value.len()] == input_value,
+                        (Mode::Match, pattern) => Regex::from_str(&pattern).unwrap().is_match(&pair.address)
+                    };
+
+                    if is_match {
                         println!("Private key: {}", pair.get_private_key_as_hex());
                         println!("Address: {}", pair.get_address_with_prefix());
                         println!("\n");
@@ -26,3 +35,4 @@ fn main() {
                 })
         });
 }
+
